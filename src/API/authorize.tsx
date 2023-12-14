4cf4fdc64f1f55c728cc4071ea46a65abf806339
정채림 <chaelimj45@gmail.com>
2023-12-14 18:27:14 +0900

    "feat: practice"

--- src/API/authorize.tsx
+++ src/API/authorize.tsx
@@ -1,15 +1,31 @@
-const client_id: string = "ada2b43ffdd04e2e9d32f125913a4731";
-const redirect_uri: string = "http://localhost:3000";
+var client_id = 'CLIENT_ID';
+var client_secret = 'CLIENT_SECRET';
 
+var authOptions = {
+  url: 'https://accounts.spotify.com/api/token',
+  headers: {
+    'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
+  },
+  form: {
+    grant_type: 'client_credentials'
+  },
+  json: true
+};
 
-export default function authorize() {
-  const generateRandomString = (length: number): string => {
-    const possible =
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    const values = crypto.getRandomValues(new Uint8Array(length));
-    return values.reduce((acc, x) => acc + possible[x % possible.length], "");
-  };
-  return <div>Authorize</div>;
+request.post(authOptions, function(error, response, body) {
+  if (!error && response.statusCode === 200) {
+    var token = body.access_token;
+  }
+});
+
+interface sum {
+    a: number;
+    b: string;
 }
+type calc2 = Pick<sum, 'a' | 'b'>;
 
+const calc: { a: number; b: string } = {
+    a: 3,
+    b: '4',
+};
 
